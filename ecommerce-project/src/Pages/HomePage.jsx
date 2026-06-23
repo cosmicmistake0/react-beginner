07cf20da2d9520faeb5fe933d8e138ca66677ad2
cosmicmistake0 <nasirumubarak61@gmail.com>
2026-06-23 18:06:01 +0100

    add server proxy for /api and /image

--- ecommerce-project/src/Pages/HomePage.jsx
+++ ecommerce-project/src/Pages/HomePage.jsx
@@ -5,17 +5,21 @@ import { Header } from "../assets/components/Header";
 import "./HomePage.css";
 export function HomePage() {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
 
   useEffect(() => {
-    axios.get("https://localhost:3000/api/products").then((response) => {
+    axios.get("/api/products").then((response) => {
       setProducts(response.data);
     });
   }, []);
+  axios.get("  /api/cart-items").then((response) => {
+    setCart(response.data);
+  });
 
   return (
     <>
       <title>Ecommerce Project</title>
-      <Header />
+      <Header cart={cart} />
 
       <div className="home-page">
         <div className="products-grid">
